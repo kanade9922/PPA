2ebb0b203b9b43e3b875f23b997cc18380ec5e74
Saurabh kanade <saurabhkanade9922@gmail.com>
2025-11-19 11:50:05 +0530

    Add files via upload

---
+++ C_Programming/EntryPoint.c
@@ -0,0 +1,13 @@
+#include "Marvellous.h"
+
+int main()
+{
+    float fValue = 10.5f;
+    float fRet = 0.0f;
+
+    fRet = CircleArea(fValue);
+
+    printf("Area of circle is : %d\n",fRet);
+
+    return 0;
+}
